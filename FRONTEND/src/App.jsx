41bcaa31c6f92d09cf23c2d5e8044c4e703d3d86
pranav-P92 <patnalapranav@gmail.com>
2024-06-01 22:45:03 +0530

    nav

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Navbar from '../Components/Navbar/Navbar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        <Navbar />
+        <div className="App">
+          <Sidebar/>
+        </div>
         </div>
     </>
   )
