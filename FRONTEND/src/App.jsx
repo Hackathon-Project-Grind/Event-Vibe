9cadf6da0bde81b3af5ad2b6aa4d07bb0f2fb63d
pranav-P92 <patnalapranav@gmail.com>
2024-06-02 07:00:56 +0530

    fixednav

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ListEvents from './components/ListEvents/ListEvents.jsx';
 import Home from './components/Home/Home.jsx'
 import Booknow from './components/Booknow/Booknow.jsx';
-
+import Header from './components/Header/Header.jsx';
 function App() {
   return (
       <div>
-
+        <Header/>
         <Booknow/>
         
       </div>
