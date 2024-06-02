36bdd4f5600e144a0193957f3e5ef75f1b83bc99
AKS VIBHAKAR <aksvibhakar@gmail.com>
2024-06-02 05:53:19 +0530

    booknow

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -5,12 +5,13 @@ import Navbar from './components/Navigation/Navbar.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ListEvents from './components/ListEvents/ListEvents.jsx';
 import Home from './components/Home/Home.jsx'
+import Booknow from './components/Booknow/Booknow.jsx';
 
 function App() {
   return (
       <div>
 
-        <Home/>
+        <Booknow/>
         
       </div>
   );
